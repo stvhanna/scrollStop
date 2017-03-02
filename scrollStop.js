7b97d72af2fa1cb8b1dded01098735448e6e70ca
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-03-02 10:26:12 -0500

    Create scrollStop.js

---
+++ scrollStop.js
@@ -0,0 +1,29 @@
+/**
+ * Run functions after scrolling has stopped
+ * @param  {Function} callback The function to run after scrolling
+ */
+var scrollStop = function ( callback ) {
+
+	// Make sure a valid callback was provided
+	if ( !callback || Object.prototype.toString.call( callback ) !== '[object Function]' ) return;
+
+	// Setup scrolling variable
+	var isScrolling;
+
+	// Listen for scroll events
+	window.addEventListener('scroll', function ( event ) {
+
+		// Clear our timeout throughout the scroll
+		window.clearTimeout( isScrolling );
+
+		// Set a timeout to run after scrolling ends
+		isScrolling = setTimeout(function() {
+
+			// Run the callback
+			callback();
+
+		}, 66);
+
+	}, false);
+
+};
